54169215138359a268c1d14a644c4d66f6ca19d3
UTGEA <UGTRE@aa.com>
2023-04-30 22:35:06 +0800

    all in

--- MCU-32bit-project/RTL_code/RTL_code_1/RTL_verilog_code/IFetch.v
+++ MCU-32bit-project/RTL_code/RTL_code_1/RTL_verilog_code/IFetch.v
@@ -1,6 +1,6 @@
 // Generator : SpinalHDL v1.8.1    git head : 2a7592004363e5b40ec43e1f122ed8641cd8965b
 // Component : IFetch
-// Git hash  : 8c6c7cf9176f84e124a944713de8a584b5d63265
+// Git hash  : 42245be0c103b6f7a97635b2c19f9bae38093db0
 
 `timescale 1ns/1ps
 
